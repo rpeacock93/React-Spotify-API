0359a73f2f07ccc19d23f0645301bd0f9f0de00f
Ryan Peacock <rpeacock@learn4life.org>
2019-02-08 16:25:02 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+let express = require("express");
+let request = require("request");
+let querystring = require("querystring");
+
+let app = express();
+
+let redirect_uri = process.env.REDIRECT_URI || "http://localhost:8888/callback";
+
+app.get("/login", function(req, res) {
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: "7c8b99aa3a594711ad2819cf8639ba97",
+        scope: "user-read-private user-read-email",
+        redirect_uri
+      })
+  );
+});
+
+app.get("/callback", function(req, res) {
+  let code = req.query.code || null;
+  let authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: "authorization_code"
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(
+          "7c8b99aa3a594711ad2819cf8639ba97" +
+            ":" +
+            "f2c9c7fa94cf46b79a1c9e319d99c9f8"
+        ).toString("base64")
+    },
+    json: true
+  };
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token;
+    let uri = process.env.FRONTEND_URI || "http://localhost:3000";
+    res.redirect(uri + "?access_token=" + access_token);
+  });
+});
+
+let port = process.env.PORT || 8888;
+console.log(
+  `Listening on port ${port}. Go /login to initiate authentication flow.`
+);
+app.listen(port);
